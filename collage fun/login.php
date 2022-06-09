569cc3718d2fdf22c3986c51a20f892683565b39
Mohd Shahnawaz <32464723+shahnawaz8@users.noreply.github.com>
2022-06-09 18:38:10 +0530

    first commit

---
+++ collage fun/login.php
@@ -0,0 +1,25 @@
+<html><head><title> User Login</title>
+    <link rel="stylesheet" href="user_login.css" >
+
+    
+    </head>
+    <body bgcolor="black">
+       
+    <center>
+        <img src="img_aavatar2.png" alt="Please wait">
+        <h1>Sarswati Istitute of ENGG & Tech</h1>
+        <form>
+            <input class="id" type="text" name="user_id" placeholder="USER ID"/><br>
+            <input class="id" type="password" name="user_password" placeholder="PASSWORD"/> <br>
+            <input class="button" type="submit" name="user_login" value="Login"/>
+            <a class="a" href="#"><input class="button2" type="submit" name="forgot_password" value="Forgot Password"/></a><br>
+            <a class="a" href="#"><input class="button2" type="submit" name="student_signup" value="Student SignUp"/></a>
+        
+        
+        </form>
+        </center>
+    
+    
+    </body>
+
+</html>
---
+++ collage fun/student_signup.php
@@ -0,0 +1,154 @@
+<html lang="en">
+    <head>
+        <style type="text/css">
+
+            <style>.text-dark[_ngcontent-c1]{color:#000}
+        </style>
+    </head>
+    
+    <body><center><table>
+        
+
+    <div _ngcontent-c1="" class="col-sm-4 mx-auto justify-content-center">
+
+    <div _ngcontent-c1="" class="card">
+        <div _ngcontent-c1="" class="text-center">
+            <img _ngcontent-c1="" class="user-avatar" height="70px" src="img_aavatar2.png" width="70px"><br>
+            <label _ngcontent-c1="" class="h5 d-none d-sm-block">Saraswati Institute Of Engg & Tech</label>
+            <label _ngcontent-c1="" class="small text-center">Saraswati College Ghaziabad, Student Portal</label>
+        </div>
+    </div>
+    <form _ngcontent-c1="" novalidate="" class="ng-invalid">
+     
+          <h4 _ngcontent-c1="" class="text-center">Student SignUp</h4>
+
+            <label _ngcontent-c1="" class="b required" for="classId">Select Your Class</label>
+            <select _ngcontent-c1="" class="form-control input-sm ng-untouched ng-pristine ng-invalid" id="classId" name="classId" ngmodel="" required="">
+                <option _ngcontent-c1="" value="">-Select Class-</option>
+            <optgroup _ngcontent-c1="" style="font-size:15px;" label="Wing-I">
+                    <!----><option _ngcontent-c1="" value="1">B.Voc.(RM)-I</option>
+                           <option _ngcontent-c1="" value="2">B.Voc.(RM)-II</option>
+                           <option _ngcontent-c1="" value="3">B.Voc.(RM)-III</option>
+                           <option _ngcontent-c1="" value="4">B.Voc.(SD)-I</option>
+                           <option _ngcontent-c1="" value="5">B.Voc.(SD)-II</option>
+                           <option _ngcontent-c1="" value="6">B.Voc.(SD)-III</option>
+                           <option _ngcontent-c1="" value="7">BA-I</option>
+                           <option _ngcontent-c1="" value="8">BA-II</option>
+                           <option _ngcontent-c1="" value="9">BA-III</option>
+                           <option _ngcontent-c1="" value="10">BCOM-I</option>
+                           <option _ngcontent-c1="" value="11">BCOM-II</option>
+                           <option _ngcontent-c1="" value="12">BCOM-III</option>
+                           <option _ngcontent-c1="" value="13">BSC-I</option>
+                           <option _ngcontent-c1="" value="14">BSC-II</option>
+                           <option _ngcontent-c1="" value="15">BSC-III</option>
+            </optgroup>
+            <optgroup _ngcontent-c1="" style="font-size:15px;" label="Wing-II">
+                           <option _ngcontent-c1="" value="16">BA-I</option>
+                <option _ngcontent-c1="" value="17">BA-II</option>
+                <option _ngcontent-c1="" value="18">BA-III</option>
+                <option _ngcontent-c1="" value="19">BCOM-I</option>
+                <option _ngcontent-c1="" value="20">BCOM-II</option>
+                <option _ngcontent-c1="" value="21">BCOM-III</option>
+                <option _ngcontent-c1="" value="22">BSC-I</option>
+                <option _ngcontent-c1="" value="23">BSC-II</option>
+                <option _ngcontent-c1="" value="24">BSC-III</option>
+            </optgroup>
+                
+            <optgroup _ngcontent-c1="" style="font-size:15px;" label="Wing-III">
+                   <option _ngcontent-c1="" value="25">B.Com(Hons.)-I</option>
+                <option _ngcontent-c1="" value="26">B.Com(Hons.)-II</option>
+                <option _ngcontent-c1="" value="27">B.Com(Hons.)-III</option>
+                <option _ngcontent-c1="" value="28">B.Sc.(Hons.) MATH-I</option>
+                <option _ngcontent-c1="" value="29">B.Sc.(Hons.) MATH-II</option>
+                <option _ngcontent-c1="" value="30">B.Sc.(Hons.) MATH-III</option>
+                <option _ngcontent-c1="" value="31">B.Sc.(Hons.) PHY.-I</option>
+                <option _ngcontent-c1="" value="32">B.Sc.(Hons.) PHY.-II</option>
+                <option _ngcontent-c1="" value="33">B.Sc.(Hons.) PHY.-III</option>
+                <option _ngcontent-c1="" value="34">B.Sc.(Non Medical)-I</option>
+                <option _ngcontent-c1="" value="35">B.Sc.(Non Medical)-II</option>
+                <option _ngcontent-c1="" value="36">B.Sc.(Non Medical)-III</option>
+                <option _ngcontent-c1="" value="37">BA(Hons)ECO-I</option>
+                <option _ngcontent-c1="" value="38">BBA CAM-I</option>
+                <option _ngcontent-c1="" value="39">BBA CAM-II</option>
+                <option _ngcontent-c1="" value="40">BBA CAM-III</option>
+                <option _ngcontent-c1="" value="41">BBA GEN-I</option>
+                <option _ngcontent-c1="" value="42">BBA GEN-II</option>
+                <option _ngcontent-c1="" value="43">BBA GEN-III</option>
+                <option _ngcontent-c1="" value="44">BCA-I</option>
+                <option _ngcontent-c1="" value="45">BCA-II</option>
+                <option _ngcontent-c1="" value="46">BCA-III</option>
+                <option _ngcontent-c1="" value="47">BCOM-I</option>
+                <option _ngcontent-c1="" value="48">BCOM-II</option>
+                <option _ngcontent-c1="" value="49">BCOM-III</option>
+                <option _ngcontent-c1="" value="50">BSC COMP-I</option>
+                <option _ngcontent-c1="" value="51">BSC COMP-II</option>
+                <option _ngcontent-c1="" value="52">BSC COMP-III</option>
+                <option _ngcontent-c1="" value="53">BSC ELE-I</option>
+                <option _ngcontent-c1="" value="54">BSC ELE-II</option>
+                <option _ngcontent-c1="" value="55">BSC ELE-III</option>
+                <option _ngcontent-c1="" value="56">BSc(Hons) Chemistry-I</option>
+                <option _ngcontent-c1="" value="57">M.A.Economics-I</option>
+                <option _ngcontent-c1="" value="58">M.A.Economics-II</option>
+                <option _ngcontent-c1="" value="59">MA ENGLISH-I</option>
+                <option _ngcontent-c1="" value="60">MA ENGLISH-II</option>
+                <option _ngcontent-c1="" value="61">MA HINDI-I</option>
+                <option _ngcontent-c1="" value="62">MA HINDI-II</option>
+                <option _ngcontent-c1="" value="63">MCOM-I</option>
+                <option _ngcontent-c1="" value="64">MCOM-II</option>
+                <option _ngcontent-c1="" value="65">MSC CHEMISTRY-I</option>
+                <option _ngcontent-c1="" value="66">MSC CHEMISTRY-II</option>
+                <option _ngcontent-c1="" value="67">MSC COMP-I</option>
+                <option _ngcontent-c1="" value="68">MSC COMP-II</option>
+                <option _ngcontent-c1="" value="69">MSC MATH-I</option>
+                <option _ngcontent-c1="" value="70">MSC MATH-II</option>
+                <option _ngcontent-c1="" value="71">MSC PHYSICS-I</option>
+                <option _ngcontent-c1="" value="72">MSC PHYSICS-II</option>
+                
+        </optgroup>
+            </select>
+            <div _ngcontent-c1="" class="form-group">
+                <label _ngcontent-c1="" class="b required" for="univRollNo">Enter Your Roll No.</label>
+                <p _ngcontent-c1="" class="small text-danger text-italic">Please Note: For 3rd year students, enter last 3 digit of your Roll No.</p>
+                <input _ngcontent-c1="" class="form-control input-sm ng-untouched ng-pristine ng-invalid" id="univRollNo" name="univRollNo" ngmodel="" placeholder="Enter Roll No." required="" type="text">
+            </div>
+            <div _ngcontent-c1="" class="form-group">
+              <label _ngcontent-c1="" class="b required" for="dob">Enter Your Date of Birth</label>
+              <input _ngcontent-c1="" class="form-control input-sm ng-untouched ng-pristine ng-invalid" dob="" id="dob" name="dob" ngmodel="" required="" type="date">
+            </div>
+            <div _ngcontent-c1="" class="form-group">
+                  <label _ngcontent-c1="" class="b required" for="nPass">Create Password</label>
+                  <input _ngcontent-c1="" class="form-control input-sm ng-untouched ng-pristine ng-invalid" id="nPass" maxlength="40" minlength="8" npass="" name="nPass" ngmodel="" placeholder="Enter Password" required="" type="password">
+                  <!---->
+              </div>
+              <div _ngcontent-c1="" class="form-group">
+                  <label _ngcontent-c1="" class="b required" for="ncPass">Confirm Password</label>
+                  <input _ngcontent-c1="" class="form-control input-sm ng-untouched ng-pristine ng-invalid" id="ncPass" name="ncPass" ncpass="" ngmodel="" placeholder="Confirm Password" required="" type="password">
+                  <!---->
+              </div>
+              <div _ngcontent-c1="" class="form-group">
+                <label _ngcontent-c1="" class="b text-danger required" for="cPass">Enter Your Valid Email Id.</label>
+
+                <input _ngcontent-c1="" class="form-control input-sm ng-untouched ng-pristine ng-invalid" email="" id="email" maxlength="50" name="email" ngmodel="" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" placeholder="Enter Email Id." required="" type="email">
+                <!---->
+                <!----><p _ngcontent-c1="" class="text-danger b" style="padding:0;margin:0;">PLEASE NOTE:</p>
+                <!----><p _ngcontent-c1="" class=" b small text-danger" style="padding:0;margin:0;">A Four Digit Code will be sent to the above Email Id. for Verification</p>
+                <!---->
+            </div>
+
+    
+        <div _ngcontent-c1="" class="card-footer">
+
+            <!----><button _ngcontent-c1="" class="btn btn-success btn-sm" type="submit" disabled="">Proceed</button>
+            <!----><a _ngcontent-c1="" class="pull-right" href="/login">Back</a>
+
+            <div _ngcontent-c1="" class="form-inline">
+                
+            </div>
+           
+        </div>
+      
+    </form>
+    <!---->
+    </div>
+        </table>    </center></body>
+</html>
